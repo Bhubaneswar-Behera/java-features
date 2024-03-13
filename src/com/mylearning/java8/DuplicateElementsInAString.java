2857cb8b209b1202c9863fdbbebb1b72297da174
Self <bhubaneswar.behera@gmail.com>
2024-03-13 12:03:25 +0530

    Java 8 QA

---
+++ src/com/mylearning/java8/DuplicateElementsInAString.java
@@ -0,0 +1,22 @@
+package com.mylearning.java8;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class DuplicateElementsInAString {
+    public static void main(String[] args) {
+        String str = "illovejavaandspring";
+
+        var duplicateElements = Arrays.stream(str.split(""))
+                .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
+                .entrySet().stream()
+                .filter(e -> e.getValue()> 1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+        System.out.println(duplicateElements);
+
+
+    }
+}
