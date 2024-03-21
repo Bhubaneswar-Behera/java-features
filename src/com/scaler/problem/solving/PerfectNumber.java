6201e7289bc4a064a9f120682045dde6bc129d33
Self <bhubaneswar.behera@gmail.com>
2024-03-21 11:59:05 +0530

    Java 8 QA

---
+++ src/com/scaler/problem/solving/PerfectNumber.java
@@ -0,0 +1,46 @@
+package com.scaler.problem.solving;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PerfectNumber {
+    public static List<Integer> getFactors(int A) {
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 1 ; i <= A/i ;i++){
+            if(A % i == 0){
+                list.add(i);
+                if(i != A/i){
+                    list.add(A/i);
+                }
+            }
+        }
+        return list;
+    }
+
+    private static int solution(int A){
+        List<Integer> lists = new ArrayList();
+
+        for(int i = 1 ; i < A;i++){
+            if(A % i == 0){
+                lists.add(i);
+            }
+        }
+
+        System.out.println(lists);
+        int sum = 0;
+        for(int i = 0 ;i< lists.size();i++){
+            sum = sum + lists.get(i);
+        }
+        if(sum == A){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(solution(6));
+        System.out.println(solution(4));
+    }
+}
---
+++ src/com/scaler/problem/solving/PrimeNumber.java
@@ -0,0 +1,30 @@
+package com.scaler.problem.solving;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeNumber {
+
+    public static int countFactor(int A){
+        int count = 0;
+
+        for(int i = 1 ; i <= A/i;i++){
+            if(A % i == 0){
+                count++;
+                if(i != A/i){
+                    count++;
+                }
+
+            }
+        }
+
+        return count;
+    }
+    public static int solution(int A){
+        return countFactor(A) == 2 ? 1 :0;
+    }
+    public static void main(String[] args) {
+        System.out.println(solution(4));
+        System.out.println(solution(6));
+    }
+}
