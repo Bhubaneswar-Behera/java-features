a592fa749e78e56902a3de62e60cf3eb14503655
Self <bhubaneswar.behera@gmail.com>
2024-03-29 14:48:37 +0530

    Java 8 QA

---
+++ src/com/scaler/introduction/to/arrays/GoodPair.java
@@ -0,0 +1,76 @@
+package com.scaler.introduction.to.arrays;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Given an array A and an integer B.
+ * A pair(i, j) in the array is a good pair if i != j
+ * and (A[i] + A[j] == B).
+ * Check if any good pair exist or not.
+ *
+ * Input 1:
+ *A = [1,2,3,4] B = 7
+ *Output : 1
+ *
+ * Input 2:
+ * A = [1,2,4] B = 4
+ *Output  :0
+ *
+ * Input 3:
+ * A = [1,2,2] B = 4
+ *Output = 0
+ *
+ */
+public class GoodPair {
+    public static int solveBrute(int[] A, int B) {
+
+        for(int i = 0 ; i < A.length;i++){
+            for(int j = 0 ; j< A.length;j++){
+                if(A[i] + A[j] == B && (i != j)) {
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static int solveOptimized(int[] A, int B) {
+           Map<Integer,Integer> hashMap = new HashMap<>();
+
+           for(int i = 0 ; i < A.length;i++){
+               int delta = B - A[i];
+               if(hashMap.containsKey(delta)){
+                   return 1;
+               }
+               hashMap.put(A[i],i );
+           }
+           return 0;
+    }
+
+    public static int solveUsingArrayList(ArrayList<Integer> A, int B) {
+        Map<Integer,Integer> hashMap = new HashMap<>();
+
+        for (int  i = 0 ; i < A.size();i++) {
+            int delta = B - A.get(i);
+            if(hashMap.containsKey(delta)){
+                return 1;
+            }
+            hashMap.put(A.get(i),i);
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        int[] A = {1,2,3,4};
+        //System.out.println(solveBrute(A, 4));
+        //System.out.println(solveOptimized(A, 7));
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println(solveUsingArrayList(list,7));
+    }
+}
---
+++ src/com/scaler/introduction/to/arrays/ReverseInARange.java
@@ -0,0 +1,45 @@
+package com.scaler.introduction.to.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ReverseInARange {
+    public static void reverse(int[] arr,int n, int l, int r) {
+        int i = l;
+        int j = r;
+        while(i < j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public ArrayList<Integer> solve(ArrayList<Integer> A, int B, int C) {
+        int i = B;
+        int j = C;
+
+        while(i < j){
+            int first = A.get(i);
+            int second = A.get(j);
+
+            int temp = A.get(i);
+            first = A.get(j);
+            second = first;
+
+
+            i++;
+            j--;
+        }
+        return A;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int length = arr.length;
+        int l = 1;
+        int r = 3;
+        reverse(arr,length,l,r);
+        System.out.println(Arrays.toString(arr));
+    }
+}
