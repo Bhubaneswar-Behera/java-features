77859663f28ada0c732be5397d01487815b30834
Self <bhubaneswar.behera@gmail.com>
2024-04-09 16:13:16 +0530

    Java 8 QA

--- src/com/scaler/intermediate/dsa/introduction/to/problem/solving/Test.java
+++ src/com/scaler/intermediate/dsa/introduction/to/problem/solving/Test.java
@@ -1,41 +1,33 @@
 package com.scaler.intermediate.dsa.introduction.to.problem.solving;
 
-public class CountPrimeNumbers {
-    public int solve(int A){
-        int count = 0;
-
-        for (int  i = 0; i <= A ;i++) {
+public class Test {
+    public static int solve(int A) {
+        int count  = 0;
+        for(int i = 0; i <= A;i++){
             if (isPrime(i)) {
                 count++;
             }
         }
         return count;
     }
-
-    private int countFactors(int A){
+    public static boolean isPrime(int A){
+        return countFactors(A) == 2 ? true : false;
+    }
+    public static int countFactors(int A){
         int count = 0;
-
-
-        for (int i = 1 ; i <= A/i ; i++) {
+        for(int i = 1 ;i <= A/i;i++){
             if(A % i == 0){
-
                 count++;
                 if(i != A/i){
                     count++;
-
                 }
             }
         }
-
         return count;
     }
-
-    private boolean isPrime(int A){
-        return countFactors(A) == 2 ? true : false;
-    }
-
     public static void main(String[] args) {
-        CountPrimeNumbers countPrimeNumbers = new CountPrimeNumbers();
-        System.out.println(countPrimeNumbers.solve(9));
+        int a = 9;
+        System.out.println(solve(a));
+
     }
 }
