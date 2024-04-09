d0fa189f9b80267f0d495db352daefe462f1552f
Self <bhubaneswar.behera@gmail.com>
2024-04-09 16:13:52 +0530

    Java 8 QA

--- src/com/scaler/intermediate/dsa/introduction/to/problem/solving/Test.java
+++ src/com/scaler/intermediate/dsa/introduction/to/problem/solving/Test.java
@@ -1,33 +1,9 @@
 package com.scaler.intermediate.dsa.introduction.to.problem.solving;
 
 public class Test {
-    public static int solve(int A) {
-        int count  = 0;
-        for(int i = 0; i <= A;i++){
-            if (isPrime(i)) {
-                count++;
-            }
-        }
-        return count;
-    }
-    public static boolean isPrime(int A){
-        return countFactors(A) == 2 ? true : false;
-    }
-    public static int countFactors(int A){
-        int count = 0;
-        for(int i = 1 ;i <= A/i;i++){
-            if(A % i == 0){
-                count++;
-                if(i != A/i){
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
+
     public static void main(String[] args) {
-        int a = 9;
-        System.out.println(solve(a));
+
 
     }
 }
