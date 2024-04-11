a7603eaa839c740b91d6f341566d1e85eb6e7236
Self <bhubaneswar.behera@gmail.com>
2024-04-11 14:53:07 +0530

    Java 8 QA

--- src/com/scaler/intermediate/dsa/sorting/basics/NobleIntegersNotDistinct.java
+++ src/com/scaler/intermediate/dsa/sorting/basics/NobleIntegersNotDistinct.java
@@ -46,14 +46,47 @@ public class NobleIntegersNotDistinct {
         }
         return answer != 0 ? 1 : -1;
     }
+
+    /**
+     * Given an integer array A, find if an integer p exists in the array such
+     * that the number of integers greater than p in the array equals p.
+     *
+     * Input : A = [3, 2, 1, 3] Output : 1
+     * Explanation : For integer 2, there are 2 greater elements in the array.
+     *
+     * @param arr
+     * @param n
+     * @return
+     */
+
+    public static int solve(int[] A) {
+        Arrays.sort(A);
+        int n = A.length;
+        int answer = 0;
+
+
+        if(A[n-1] == 0){
+            return 1;
+        }
+
+        for(int i =n -2 ; i>=0;i--){
+            if(A[i] != A[i+1]){
+                if((n-i-1) == A[i]){
+                    answer++;
+                }
+            }
+        }
+        return answer > 0 ? 1 : -1;
+
+    }
     public static void main(String[] args) {
         int[] A = {-3,0,2,2,5,5,5,5,8,8,10,10,10,14};
         int n = A.length;
-        //System.out.println(getNobleIntegerCountBrute(A,n));
-        //System.out.println(getNobleIntegerCount(A,n));
+        System.out.println(getNobleIntegerCountBrute(A,n));
+        System.out.println(getNobleIntegerCount(A,n));
 
         int[] A1 = {5,6,2};
-        System.out.println(getNobleIntegerCount(A1,A1.length));
+        System.out.println(solve(A1));
 
 
     }
