7365df8db135c106f42f7623ac3b199db653bd2d
Self <bhubaneswar.behera@gmail.com>
2024-04-17 11:28:25 +0530

    Bit manipulation basics

---
+++ src/com/scaler/intermediate/dsa/strings/SimpleReverse.java
@@ -0,0 +1,26 @@
+package com.scaler.intermediate.dsa.strings;
+
+/**
+ * Given a string A,
+ * you are asked to reverse the string and return the reversed string.
+ *
+ * Input: A = "scaler"
+ * Output: relacs"
+ */
+public class SimpleReverse {
+
+    public static String solve(String A) {
+        int n = A.length();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i = n -1 ;i >= 0;i--){
+            stringBuilder.append(A.charAt(i));
+        }
+
+        return stringBuilder.toString();
+    }
+    public static void main(String[] args) {
+        String A = "scaler";
+        System.out.println(solve(A));
+    }
+}
