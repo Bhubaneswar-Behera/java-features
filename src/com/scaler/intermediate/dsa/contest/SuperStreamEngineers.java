47fd37c5403d68b0a03e663b8f2a635e337a522b
Self <bhubaneswar.behera@gmail.com>
2024-04-21 17:54:24 +0530

    Interview problems

---
+++ src/com/scaler/intermediate/dsa/contest/SuperStreamEngineers.java
@@ -0,0 +1,34 @@
+package com.scaler.intermediate.dsa.contest;
+
+public class SuperStreamEngineers {
+
+    public static int solve(int[] A, int k, int C) {
+        int n = A.length;
+        int sum = 0;
+        double average = 0;
+
+        for (int i = 0; i < k; i++) {
+            sum = sum + A[i];
+        }
+        average = sum / k;
+
+        if (average < C) {
+            return 1;
+        }
+        for (int i = 1; i <= n - k; i++) {
+            int j = i + k - 1;
+            sum = sum + A[j] - A[i - 1];
+            average = sum / k;
+            if (average < C) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        int[] A = {30,25,40,35,20,45,50,55,22,18,15};
+        int B = 3;
+        int C  = 30;
+        System.out.println(solve(A,B, C));
+    }
+}
