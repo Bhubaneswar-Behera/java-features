671a205734329a7f666168c0c702fc9b0940ad81
Self <bhubaneswar.behera@gmail.com>
2024-05-01 00:25:08 +0530

    Bit Manipulation

---
+++ src/com/scaler/advanced/dsa/module4/bit/maniplutaion/one/SetBit.java
@@ -0,0 +1,31 @@
+package com.scaler.advanced.dsa.module4.bit.maniplutaion.one;
+
+/**
+ * You are given two integers A and B.
+ * Set the A-th bit and B-th bit in 0,
+ * and return output in decimal Number System.
+ *
+ * Input: A = 3 B = 5
+ * Output : 40
+ *
+ * Explanation :
+ * The binary expression is 101000 which is 40 in decimal.
+ */
+public class SetBit {
+
+    public static int solve(int A, int B) {
+        int answer = 0;
+        //set Ath Bit
+        answer = answer | (1 << A);
+
+        //set Bth Bit
+        answer = answer | (1 << B);
+        return answer;
+
+    }
+    public static void main(String[] args) {
+        int A = 3;
+        int B = 5;
+        System.out.println(A,B);
+    }
+}
