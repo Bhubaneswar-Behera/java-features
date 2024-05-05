5427017661bfd855daeb8dc0fec7b5491769e068
Self <bhubaneswar.behera@gmail.com>
2024-05-05 19:15:23 +0530

    Strategy Pattern

---
+++ src/com/designpatterns/behavioral/strategy/MainApp.java
@@ -0,0 +1,22 @@
+package com.designpatterns.behavioral.strategy;
+
+/*
+ * Video Reference: https://youtu.be/Nrwj3gZiuJU
+ */
+public class MainApp {
+    public static void main(String[] args) {
+
+        PaymentService paymentService = new PaymentService();
+        // The strategy can now be easily picked at runtime
+
+        paymentService.setStrategy(new PaymentByCreditCard());
+        paymentService.processOrder(100);
+
+        System.out.println("==========================================");
+
+        paymentService.setStrategy(new PaymentByPayPal());
+        paymentService.processOrder(100);
+
+    }
+
+}
---
+++ src/com/designpatterns/behavioral/strategy/PaymentService.java
@@ -0,0 +1,24 @@
+package com.designpatterns.behavioral.strategy;
+
+public class PaymentService {
+
+    private int cost;
+    private boolean includeDelivery = true;
+
+    private PaymentStrategy strategy;
+
+    public void processOrder(int cost) {
+        this.cost = cost;
+        strategy.collectPaymentDetails();
+        if (strategy.validatePaymentDetails()) {
+            strategy.pay(getTotal());
+        }
+    }
+
+    private int getTotal() {
+        return includeDelivery ? cost + 10 : cost;
+    }
+    public void setStrategy(PaymentStrategy strategy) {
+        this.strategy = strategy;
+    }
+}
