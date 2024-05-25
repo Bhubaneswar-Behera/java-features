f7d5b48f5227a96dada7d0f4abf85f4613f2c7e2
Self <bhubaneswar.behera@gmail.com>
2024-05-25 14:35:25 +0530

    OOPS 2

--- src/com/scaler/advanced/dsa/module5/oops/construcor/inheritance/polymorphism/seven/Client.java
+++ src/com/scaler/advanced/dsa/module5/oops/construcor/inheritance/polymorphism/seven/Client.java
@@ -2,11 +2,12 @@ package com.scaler.advanced.dsa.module5.oops.construcor.inheritance.polymorphism
 
 public class Client {
     public static void main(String[] args) {
-        C obj = new P();
+        //Uncomment the below code and decide the output
+        /*C obj = new P();
         System.out.println(obj.d1);
         System.out.println(obj.d);
         obj.fun1();
         obj.fun();
-        obj.sfun();
+        obj.sfun();*/
     }
 }
