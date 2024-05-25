a6e69243f07bc2466ed1b99bd2b293370c1c3ccc
Self <bhubaneswar.behera@gmail.com>
2024-05-25 21:36:21 +0530

    Maths2

--- src/com/scaler/advanced/dsa/module4/maths/two/combinatorics/basics/PascalTriangle.java
+++ src/com/scaler/advanced/dsa/module4/maths/two/combinatorics/basics/PascalTriangle.java
@@ -34,6 +34,8 @@ public class PascalTriangle {
             ncr[i][i] = 1;
             for (int j = 1; j < i ; j++) {
                 ncr[i][j] = ncr[i-1][j-1] + ncr[i-1][j];
+                //If mod is needed then do the below
+                //ncr[i][j] = (ncr[i-1][j-1] + ncr[i-1][j])%A;
             }
         }
         return ncr;
