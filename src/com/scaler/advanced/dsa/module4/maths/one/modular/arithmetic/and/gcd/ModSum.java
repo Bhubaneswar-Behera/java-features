ba7fdce1033e4dbaabaa1dee3a4e9b82a23f2cec
Self <bhubaneswar.behera@gmail.com>
2024-05-25 18:36:55 +0530

    Maths1

--- src/com/scaler/advanced/dsa/module4/maths/one/modular/arithmetic/and/gcd/ModSum.java
+++ src/com/scaler/advanced/dsa/module4/maths/one/modular/arithmetic/and/gcd/ModSum.java
@@ -1,5 +1,6 @@
 package com.scaler.advanced.dsa.module4.maths.one.modular.arithmetic.and.gcd;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,6 +21,7 @@ public class ModSum {
         int n = A.length;
         long result = 0;
         Map<Integer,Integer> hashMap = new HashMap<>();
+
         //count the occurrence
         for (int i = 0; i < n ; i++) {
             hashMap.put(A[i],hashMap.getOrDefault(A[i],0)+1);
@@ -37,8 +39,36 @@ public class ModSum {
 
         return (int) result;
     }
+    public static int solve(ArrayList<Integer> A) {
+        int n = A.size();
+        long answer = 0;
+        Map<Integer,Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            hashMap.put(A.get(i) , hashMap.getOrDefault(A.get(i) , 0) + 1);
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (Map.Entry<Integer,Integer> mapEntry : hashMap.entrySet()) {
+                int element = mapEntry.getKey();
+                int countOfElement = mapEntry.getValue();
+
+                answer = (answer + ((long) A.get(i) % element) * countOfElement) % 1000000007;
+
+            }
+
+        }
+
+        return (int) answer;
+    }
     public static void main(String[] args) {
         int[] A =  {1, 2, 3};
         System.out.println(solve(A));
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        System.out.println(solve(arrayList));
     }
 }
