e65a6050e8862e295e9154ba7b4d34ed6e7ca9f8
Self <bhubaneswar.behera@gmail.com>
2024-05-26 19:31:00 +0530

    Maths3

--- src/com/scaler/advanced/dsa/module4/maths/three/prime/numbers/SmallestPrimeFactor.java
+++ src/com/scaler/advanced/dsa/module4/maths/three/prime/numbers/SmallestPrimeFactor.java
@@ -12,13 +12,14 @@ import java.util.Arrays;
 public class SmallestPrimeFactor {
     public static int[] smallestPrimeFactor(int n){
         int[] spf = new int[n+1];
+
         for (int i = 2; i <= n ; i++) {
             spf[i] = i;
         }
 
         for (int i = 2; i * i <= n ; i++) {
             if (spf[i] == i) {
-                for (int j = i*i; j <=n ; j = j + i) {
+                for (int j = i * i; j <= n ; j = j + i) {
                   if (spf[j] == j) {
                       spf[j] = i;
                   }
