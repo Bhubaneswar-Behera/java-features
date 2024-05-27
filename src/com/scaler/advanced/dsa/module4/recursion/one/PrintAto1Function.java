4bd23f41e35584610348745d6415a0c5aba18dd8
Self <bhubaneswar.behera@gmail.com>
2024-05-27 17:53:29 +0530

    Recursion

---
+++ src/com/scaler/advanced/dsa/module4/recursion/one/PrintAto1Function.java
@@ -0,0 +1,28 @@
+package com.scaler.advanced.dsa.module4.recursion.one;
+
+/**
+ * You are given an integer A, print A to 1 using recursion.
+ * Note :- After printing all the numbers from A to 1, print a new line.
+ *
+ * Input : 10
+ * Output: 10 9 8 7 6 5 4 3 2 1
+ * Explanation : Print 10 to 1 space separated integers.
+ *
+ * Input : 5
+ * Output : 5 4 3 2 1
+ * Explanation : Print 5 to 1 space separated integers.
+ */
+public class PrintAto1Function {
+    public static void solve(int A) {
+        if (A < 1) {
+            System.out.println();
+            return;
+        }
+        System.out.print(A +" ");
+        solve(A - 1);
+    }
+    public static void main(String[] args) {
+        int A = 9;
+        solve(A);
+    }
+}
