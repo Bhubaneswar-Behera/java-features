90f2d4353c6a3a4ac50f5fa9901ed5c99e8f2665
Self <bhubaneswar.behera@gmail.com>
2024-06-04 21:24:44 +0530

    HashMap Introduction

--- src/com/scaler/advanced/dsa/module5/hashing/introduction/ScalerFindLeastAppearance.java
+++ src/com/scaler/advanced/dsa/module5/hashing/introduction/ScalerFindLeastAppearance.java
@@ -1,16 +1,31 @@
 package com.scaler.advanced.dsa.module5.hashing.introduction;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  *
  * You have the list of ID's , where each ID(integer) represent a learned who
  * tried a contest. A leader's ID shows up X times  if they try x context.
- * Write a programme that looks at the list and fnd out how may distinct Student have tried the least number of
- * contests.
+ * Write a programme that looks at the list and fnd out how many distinct
+ * Student have tried the least number of contests.
+ * Input  : A = { 101,102,103,101,102,101,104,105,106,105,105 }
+ * Output : 103,104,106
+ * Explanation :
+ *  Element     Frequency
+ *  101     ->      3
+ *  102     ->      2
+ *  103     ->      1
+ *  104     ->      1
+ *  105     ->      3
+ *  106     ->      1
+ *  3 keys whose value is 1. So the output is  103,104,106
+ *
+ *
  */
-public class FindLeastAppearance {
+public class ScalerFindLeastAppearance {
     public static int findLeastApproach1(int[] A){
         int n = A.length;
 
