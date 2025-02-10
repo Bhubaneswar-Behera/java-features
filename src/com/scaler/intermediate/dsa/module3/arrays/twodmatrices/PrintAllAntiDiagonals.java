475676e5bdf760b327d66b417d7f9a49d2dfef62
Self <bhubaneswar.behera@gmail.com>
2025-02-10 21:19:12 +0530

    matrix

--- src/com/scaler/intermediate/dsa/module3/arrays/twodmatrices/PrintAllAntiDiagonals.java
+++ src/com/scaler/intermediate/dsa/module3/arrays/twodmatrices/PrintAllAntiDiagonals.java
@@ -5,9 +5,10 @@ package com.scaler.intermediate.dsa.module3.arrays.twodmatrices;
  * The vacant spaces in the grid should be assigned to 0.
  *
  * Input:
- * m[3][4] =  {{1, 2,  3,  4},
- *             {5, 6,  7,  8},
- *             {9, 10, 11, 12}}
+ * m[3][4] =  { {1, 2,  3,  4},
+ *              {5, 6,  7,  8},
+ *              {9, 10, 11, 12}
+ *             }
  * Output:
  *          1 0 0
  *          2 5 0
