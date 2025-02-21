dcd7c11d9e38e6a2916fd3772c78f00c52637bbf
Self <bhubaneswar.behera@gmail.com>
2025-02-21 15:18:32 +0530

    math

--- src/com/scaler/advanced/dsa/advanceddsa1/module5/maths/two/combinatorics/basics/PascalTriangle.java
+++ src/com/scaler/advanced/dsa/advanceddsa1/module5/maths/two/combinatorics/basics/PascalTriangle.java
@@ -41,7 +41,7 @@ public class PascalTriangle {
         return ncr;
     }
     public static void main(String[] args) {
-        int A = 3;
+        int A = 5;
         int[][] result = solve(A);
 
         for (int[] a : result) {
