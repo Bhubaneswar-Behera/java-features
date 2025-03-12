5cdf281545a2a4998e0e65b3570c60f40229e34d
Self <bhubaneswar.behera@gmail.com>
2025-03-12 22:19:07 +0530

    Two Pointers

--- src/com/scaler/advanced/dsa/advanceddsa2/module6/twopointers/PairsWithGivenSumII.java
+++ src/com/scaler/advanced/dsa/advanceddsa2/module6/twopointers/PairsWithGivenSumII.java
@@ -74,10 +74,28 @@ public class PairsWithGivenSumII {
         }
         return (int) count; // Cast count back to int before returning
     }
+    public static int countTwoSumPairs(int[] A, int B) {
+        int i = 0;
+        int j = A.length - 1;
+        int count = 0;
+        while (i < j) {
+            if(A[i] + A[j] == B){
+                count++;
+                i++;
+                j--;
+            } else if (A[i] + A[j] < B) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
         int[] A = {1, 5, 7, 10};
         int B = 8;
         System.out.println(solveWithOutDuplicates(A,B));
+        System.out.println(countTwoSumPairs(A,B));
 
 
         int[] A1 = {1, 1, 1};
