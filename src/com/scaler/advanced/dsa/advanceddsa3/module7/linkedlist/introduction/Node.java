04a8c2257e5543b937479700e3d20dbed48ff161
Self <bhubaneswar.behera@gmail.com>
2025-03-15 20:58:18 +0530

    LinkedList

---
+++ src/com/scaler/advanced/dsa/advanceddsa3/module7/linkedlist/introduction/Node.java
@@ -0,0 +1,11 @@
+package com.scaler.advanced.dsa.advanceddsa3.module7.linkedlist.introduction;
+
+public class Node {
+    int data;
+    Node next;
+    Node(int x) {
+        data = x;
+        next = null;
+
+    }
+}
