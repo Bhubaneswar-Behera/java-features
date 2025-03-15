b21f64661f0da4578dd4ac5de7ce6f131209dbe7
Self <bhubaneswar.behera@gmail.com>
2025-03-15 21:33:04 +0530

    LinkedList

--- src/com/scaler/advanced/dsa/advanceddsa3/module7/linkedlist/introduction/LinkedList.java
+++ src/com/scaler/advanced/dsa/advanceddsa3/module7/linkedlist/introduction/LinkedList.java
@@ -21,20 +21,58 @@ public class LinkedList {
         return false;
     }
     //insert a new node with data v at index p in the linked list
-    public Node insertAtPosition(int position, int data, Node head) {
+    public Node insertAtIndex(int index, int data, Node head) {
         Node newNode = new Node(data);
         Node temp = head;
-        if (position == 0) {
+        if (index == 0) {
             newNode.next = head;
             head = newNode;
             return head;
         }
 
-        for (int i = 1; i < position ; i++) {
+        for (int i = 1; i < index ; i++) {
             temp = temp.next;
         }
         newNode.next = temp.next;
         temp.next = newNode;
+        return head;
+    }
+    //delete the first occurrence of the value x in the linked list
+    //id value is not present then return the linked list as it is
+    public Node deleteNode( int x, Node head) {
+        if (head == null) {
+            return null;
+        }
+        if (head.data == x) {
+            head = head.next;
+            return head;
+        }
+        Node tempNode = head;
+        while (tempNode.next != null) {
+            if (tempNode.next.data == x) {
+                tempNode.next = tempNode.next.next;
+                break;
+            }
+            tempNode = tempNode.next;
+        }
+        return head;
+    }
+    //Delete all elements with value x from the linked list
+    public Node deleteAllOccurances(Node head, int x) {
+       if(head == null){
+           return head;
+       }
+       if(head.data == x){
+           head = head.next;
+       }
+        Node temp = head;
+        while (temp != null && temp.next != null) {
+            if (temp.next.data == x) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
         return head;
     }
 }
